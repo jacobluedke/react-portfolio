b13ff6d9b1ea6132d6b1cc151c3a6a442265acd8
Jake Luedke <rajake18@gmail.com>
2022-03-01 23:47:37 -0700

    had to re clone the repo because the connection to github had problems

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      {/* <Footer /> */}
+    </div>
+  );
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        These are languages that I have experience with
+
+        There are the types of applications/functionalities that I have built/worked with
+
+        Link to resume
+      </p>
+    </div>
+  );
+}
